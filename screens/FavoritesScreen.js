f8698e043ad64d9110af510d4ca19dd9119a513c
kalinadukova <kalina984@abv.bg>
2022-10-17 10:54:10 +0300

    added nested navigators

---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function FavoritesScreen() {
+  return (
+    <View>
+      <Text>The favorites Screen</Text>
+    </View>
+  );
+}
+
+export default FavoritesScreen;
